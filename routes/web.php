8d6b6a65f85e312d7f009c81711abe2ba9331175
KAMRONBEK <kamuranbek1998@gmail.com>
2026-03-08 06:34:31 +0500

    Refactor root route to redirect to API endpoint instead of returning JSON response.

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return response()->json([
-        'name' => config('app.name'),
-        'api' => url('/api/v1'),
-    ]);
-});
+Route::get('/', fn () => redirect('/api'));

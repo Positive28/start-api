8b96051f208ad5533cac707502cb52d214709f33
Amin <wazkeee@gmail.com>
2026-03-18 04:27:25 +0500

    register minor

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 use OpenApi\Annotations as OA;
 
 /**
